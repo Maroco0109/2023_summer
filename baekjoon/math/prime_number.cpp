5c623e1da539aaf2a8c63dcf2cd3ac2833b74c25
Maroco0109 <luigi1215@naver.com>
2023-06-20 16:44:44 +0900

    20230620

--- baekjoon/math/prime_number.cpp
+++ baekjoon/math/prime_number.cpp
@@ -1,21 +1,31 @@
 #include<iostream>
 #include<cmath>
+/*
 bool prime_num(int n){
-    double a = sqrt(n);
-    if(n == 1)
-        return false;
-    for(int i = 2; i <= a; i++){
+    for(int i = 2; i <= sqrt(n); i++){
         if(n % i == 0)
             return false;
     }
     return true;
 }
+*/
+void prime_num2(int n){
+    bool *prime = new bool[n+1];
 
+    for(int i = 0; i <= n; i++)
+        prime[i] = true;
+
+    for(int i = 2; i <= n; i++){
+        if(prime[i]){
+            std::cout<<i<<'\n';
+            for(int j = i*2; j <= n; j += i)
+                prime[j] = false;
+        }
+    }
+}
 int main(){
     int m, n;
     scanf("%d %d", &m, &n);
-    for(int i=m; i<=n; i++){
-        if(prime_num(i))
-            std::cout<<i<<std::endl;
-    }
+    prime_num2(n);
+
 }
